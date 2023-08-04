6f4822a5c30b8c583aa864d21b6467899ae0740f
Md Luqman Ali <113300352+Md-Luqman-Ali@users.noreply.github.com>
2023-08-04 15:56:41 +0530

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -16,7 +16,7 @@ st.markdown ('## Iris Species Prediction' )
 sepal_length = st.number_input ('sepal length (cm) ')
 sepal_width = st.number_input (' sepal width (cm)')
 petal_length =st.number_input( 'petal length (cm)')
-petal_width - st.number_input ('petal width (cm)')
+petal_width = st.number_input ('petal width (cm)')
 
 #Predict button
 if st.button(' Predict'):
